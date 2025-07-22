d609402b29d46fc697487e05be74854bd2c28de3
Adam Sousa <adamsts2002@gmail.com>
2025-07-22 00:05:14 -0300

    adicionado exemplos de promises

---
+++ 2 - Intermediário (JavaScript, Node.js, React)/10. Assync-Await/promise-tarefas.js
@@ -0,0 +1,78 @@
+/*Imagine que sua mãe te deixou uma lista de tarefas para o dia. Você precisa, nessa ordem:
+
+1. Andar com o cachorro.
+2. Arrumar a cozinha.
+3. Tirar o lixo pra fora.
+
+Você não pode passar para a próxima tarefa se não concluir a atual.*/
+
+
+/*function andarCachorro() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Você passeou com o cachorro!');
+        }, 1500);   
+    });
+};*/
+
+function andarCachorro() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+            let tarefaConcluida = true;
+
+            if (tarefaConcluida) {
+                resolve('Você passeou com o cachorro!');
+            }else {
+                reject('Você NÃO passeou com o cachorro.')
+            }
+        }, 1500);   
+    });
+};
+
+function arrumarCozinha() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+            let tarefaConcluida = false;
+
+            if (tarefaConcluida) {
+                resolve('Você arrumou a cozinha!');
+            } else {
+                reject('Você NÃO arrumou a cozinha.')
+            }
+        }, 2500);
+    });
+};
+
+function tirarLixo() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+            let tarefaConcluida = true;
+
+            if (tarefaConcluida) {
+                resolve('Você tirou o lixo pra fora!');
+            }else {
+                reject('Você NÃO tirou o lixo.')
+            }
+
+        }, 1000);
+    });
+};
+
+andarCachorro()
+  .then((value) => {
+    console.log(value);
+    return arrumarCozinha;
+  })
+  .then((value) => {
+    console.log(value);
+    return tirarLixo;
+  })
+  .then((value) => {
+    console.log(value);
+    console.log("Tarefas concluídas!");
+  })
+  .catch((error => console.error(error)
+  ));
